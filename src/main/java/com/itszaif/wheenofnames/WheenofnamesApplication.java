bfbee369f1b879bb98a250da1f27302d456f56a6
Zafrullah Syed <zafrullahmehdi@gmail.com>
2021-09-25 00:36:17 +0200

    added maven project

---
+++ src/main/java/com/itszaif/wheenofnames/WheenofnamesApplication.java
@@ -0,0 +1,13 @@
+package com.itszaif.wheenofnames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheenofnamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheenofnamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itszaif/wheenofnames/WheenofnamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itszaif.wheenofnames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheenofnamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
